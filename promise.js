7030db1e5be8872f6d78164728824f68c37f4f6e
hadinosaurus <hadi@nugroho.com>
2022-09-16 21:04:30 +0700

    Update

--- promise.js
+++ promise.js
@@ -1,8 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
-
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+const promiseOutput = async (emosi) => {
+  try {
+  ixx = await promiseTheaterIXX();
+  vgc = await promiseTheaterVGC();
+  let counter = 0;
+    return new Promise((resolve) => {
+      counter += ixx.filter((item) => item.hasil == emosi).length;
+      counter += vgc.filter((item) => item.hasil == emosi).length;
+      resolve(counter);
+    });
+  } 
+  catch (err) {
+    console.log(err);
+  }
+};
 module.exports = {
   promiseOutput,
 };
